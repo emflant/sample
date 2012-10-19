ae13ed37cadd98daa4808f47b99c9ce6447bae71
emflant <emflant@gmail.com>
2012-10-19 18:01:48 +0900

    dir test

---
+++ 201210/new4.js
@@ -0,0 +1,11 @@
+{
+	"cols":[
+		{"id":"","pattern":"","label":"Topping","type":"string"},
+		{"id":"","pattern":"","label":"Slices","type":"number"}
+	],
+	
+	"rows":[
+		{"c":[{"v":"Mushrooms"},{"v":"3"}]},
+		{"c":[{"v":"Mushrooms"},{"v":"3"}]}
+	]
+}
