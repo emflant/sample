a7e4f50c3c6b3bc2e9f4dc554503b22acc753e39
emflant <emflant@gmail.com>
2012-10-19 13:51:46 +0900

    test commit

---
+++ new2.js
@@ -0,0 +1,28 @@
+{
+	"cols":[
+		{"id":"","pattern":"","label":"Topping","type":"string"},
+		{"id":"","pattern":"","label":"Slices","type":"number"}
+	],
+	
+	"rows":[
+		{"c":[{"v":"Mushrooms"},{"v":"3"}]},
+		{"c":[{"v":"Mushrooms"},{"v":"3"}]}
+	]
+}
+	
+	
+	
+{
+	"cols": [
+		{"id":"","label":"Topping","pattern":"","type":"string"},
+		{"id":"","label":"Slices","pattern":"","type":"number"}
+	],   
+	
+	"rows": [
+		{"c":[{"v":"Mushrooms","f":null},{"v":3,"f":null}]},
+		{"c":[{"v":"Onions","f":null},{"v":1,"f":null}]},
+		{"c":[{"v":"Olives","f":null},{"v":2,"f":null}]},
+		{"c":[{"v":"Zucchini","f":null},{"v":1,"f":null}]},
+		{"c":[{"v":"Pepperoni","f":null},{"v":2,"f":null}]}
+	] 
+}
