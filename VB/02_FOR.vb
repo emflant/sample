067d4ba01d78ba84fb7e5e1d83a5ff6ea7443866
emflant <emflant@gmail.com>
2015-09-30 13:19:24 +0900

    소스수정

--- VB/02_FOR.vb
+++ VB/02_FOR.vb
@@ -1,23 +1,21 @@
-Sub 단추1_Click()
+Sub 단추2_Click()
+    
+    ' 일반적인 for 문법
+    For i = 1 To 2
+    
+        For j = 1 To 3
+        
+            ' 출력
+            MsgBox i & " * " & j & " = " & (i * j)
+            
+            ' 4 이상이면 loop break
+            If i * j >= 4 Then
+                MsgBox "break = Exit For"
+                Exit For
+            End If
+            
+        Next j
+        
+    Next i
 
-    ' 변수선언
-    ' Byte, Boolean, Integer, Long, Currency, Single, Double, Decimal(현재 지원 안됨), Date, String(가변 길이)
-    Dim vGrade As String
-    Dim vScore As Integer
-    Dim vResult As VbMsgBoxResult
-    
-    vResult = MsgBox("123", vbOKCancel, "확인")
-    
-    ' 결과값 확인.
-    MsgBox vResult
-    
-    ' if 문법
-    If VbMsgBoxResult.vbOK = vResult Then
-        MsgBox "vbOK...."
-    ElseIf VbMsgBoxResult.vbIgnore = vResult Then
-        MsgBox "vbIgnore...."
-    Else
-        MsgBox "etc...."
-    End If
-    
 End Sub
