f8b5b6f30bee1f10adb64aecdf8a864700d7e0df
emflant <emflant@gmail.com>
2018-10-16 23:58:28 +0900

    .

--- R/test08.R
+++ R/test08.R
@@ -1,25 +1,33 @@
 # install.packages("dplyr")
 # install.packages("readr")
-library(readr)
+
 
 # Sys.setlocale(category = "LC_CTYPE", locale = "ko_KR.UTF-8")
 # par(family="NanumGothic")
-Sys.setlocale(category = "LC_CTYPE", locale = "ko_KR.UTF-8")
+
+library(readr)
 apart_201801 <- read_csv("D:/workspace/data/apart_201801.csv")
 
 apart_201802 <- read_csv("D:/workspace/data/apart_201802.csv")
-apart_201802
+#apart_201802
 apart <- rbind(apart_201801, apart_201802) 
 apart
 
 library(dplyr)
 library(stringr)
 filter(apart, 시군구 == "강원도 강릉시 견소동")
-filter(apart, str_detect(시군구,"답십리"))
+filter(apart, str_detect(시군구,"장안동"))
 r1 <- filter(apart, str_detect(단지명,"^은마") & str_detect(시군구,"대치동"))
 
 r1 <- apart %>%
-    filter(str_detect(단지명,"^은마") & str_detect(시군구,"대치동"))
+    filter(str_detect(단지명,"^은마") & 
+               str_detect(시군구,"대치동") &
+               `전용면적(㎡)` == 76.79)
+
+r1$계약년월 <- factor(r1$계약년월)
+
+boxplot(`거래금액(만원)` ~ 계약년월, data = r1)
+r1
 
 r2 <- apart %>%
     group_by(시군구) %>%
---
+++ R/test09.R
@@ -0,0 +1,69 @@
+install.packages("mlbench")
+library(help="mlbench")
+library(mlbench)
+data(Ozone)
+plot(Ozone$V8, Ozone$V9, type = "p")
+
+plot(Ozone$V8, Ozone$V9, 
+     type = "p", # p/l/b/o/n
+     xlab = "Sandburg Temperature",
+     ylab = "El Monte Temperature",
+     main = "Ozone", 
+     pch = 1, # 점의 종류 
+     cex = 1, # 점의 크기
+     col="#FF0000",
+     xlim = c(0, 100),
+     ylim = c(0, 90)
+     )
+
+cars
+plot(cars)
+lines(lowess(cars))
+
+
+plot(cars)
+abline(a=-5, b=3.5, col="red")
+abline(h=mean(cars$dist), lty=2)
+abline(v=mean(cars$speed), lty=2)
+
+curve(sin, 0, 2*pi)
+
+# 
+m <- lm(dist ~ speed, data = cars) # Linear Models
+plot(cars, xlim = c(0,30))
+abline(m)
+# interval=c("none", "confidence", "prediction")
+p <- predict(m, interval="confidence")
+p <- predict(m, interval="prediction")
+p
+
+
+iris$Sepal.Width
+median(iris$Sepal.Width)
+boxplot(iris$Sepal.Width)
+boxplot(iris$Sepal.Width, notch = TRUE) # 중앙값에 대한 신뢰 구간이 오목하게 그려진다
+boxstats <- boxplot(iris$Sepal.Width)
+boxstats2 <- boxplot(iris$Sepal.Width, notch = TRUE)
+boxstats
+boxstats$stats
+# [1,]  2.2  lower whisker
+# [2,]  2.8  lower hinge 제1사분위수(25%에 해당하는 값)
+# [3,]  3.0  median 중앙값(75%에 해당하는 값)
+# [4,]  3.3  upper hinge  제3사분위수(75%에 해당하는 값)
+# [5,]  4.0  upper whisker
+
+sv <- subset(iris, Species=="setosa" | Species=="versicolor")
+sv$Species <- factor(sv$Species)
+boxplot(Sepal.Width ~ Species, data=sv, notch=TRUE)
+
+# freq 지정하지 않는 경우, 구간별 데이터의 개수(즉, 빈도)
+hist(iris$Sepal.Width) 
+
+# freq=FALSE면 다음 코드에서 보다시피 각 구간의 확률 밀도가 그려진다. 
+hist(iris$Sepal.Width, freq=FALSE) 
+# 확률 밀도므로 막대 너비의 합이 1이 된다.
+h <- hist(iris$Sepal.Width, freq=FALSE) 
+sum(h$density) * 0.2  # h$breaks를 보면 막대의 너비가 0.2임을 알 수 있다.
+
+
+lines(density(iris$Sepal.Width))
