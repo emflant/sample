68776b7aea4ba8d2e77c8623e85942dd27a0b23d
emflant <emflant@gmail.com>
2018-12-30 00:31:32 +0900

    .

--- R/file_system/list_files.R
+++ R/file_system/list_files.R
@@ -59,10 +59,11 @@ OlsonNames()
 file.info(c("A.txt", "C.txt"))
 
 
-R.home()
+
 
 ############################
-# 아래 내용 추가함. R.home() / ⁨etc/Renviron 파일에,
+# 아래 내용 추가함. R.home()/⁨etc/Renviron 파일에,
+file.path(R.home(), 'etc', 'Renviron')
 ### default time zone
 # TZ="Asia/Seoul"
 ############################
@@ -72,3 +73,78 @@ Sys.time()
 Sys.timezone()
 Sys.Date()
 Sys.getenv()
+
+
+
+120000 * 1000
+
+getwd()
+
+dir.exists(list.files())
+rmarkdown::render()
+list.files(getwd(), full.names = T)
+list.files(getwd(), full.names = T)[str_detect(list.files(), '.html$')]
+
+str_view(list.files(getwd(), full.names = T), '.html$')
+
+options("scipen" = 100)
+
+
+Sys.getenv('R_WD')
+
+
+
+
+dd <- system('ls -al', intern = T)
+as_tibble(dd)
+as_tibble(dd) %>% 
+  filter(str_detect(value, 'index\\.'))
+
+dd_t <- read_fwf(dd, fwf_empty(dd))
+dd_t %>% 
+  filter(str_detect(X9, 'index.+html'))
+
+read_lines('index.html', n_max = 10)
+
+read_file('index.Rmd')
+system.time(read_lines('index.html', n_max = 10))
+
+
+system.time(read_lines('index.html', n_max = 10, skip = 200))
+read_lines('index.html', n_max = 10, skip = 200)
+
+
+system.time(read_lines('index.html'))
+dd <- read_lines('index.html')
+
+dd_t <- as_tibble(dd)
+
+dd_t[c(31:40),]
+
+
+library(tidyverse)
+
+t_lines
+getwd()
+
+dd <- list.files(full.names = T, recursive = T) # 61
+dd
+
+
+### 특정폴더 하위의 파일들의 파일내용을 tibble 로 리턴.
+get_file_text <- function (p = "."){
+  dd <- list.files(p, full.names = T, recursive = T)
+  t_lines <- tibble(file = character(), line_number = double(), source = character())
+  
+  for(f_name in dd){
+    d1 <- read_lines(f_name)
+    t_file_source = tibble(file = rep(f_name, length(d1)), line_number = seq(length(d1)), source = d1)
+    t_lines <- bind_rows(t_lines, t_file_source)
+  }
+  t_lines # return
+}
+
+ddd <- get_file_text()
+d1 <- ddd %>%
+  filter(str_detect(source, "encoding"))
+View(d1)
--- R/file_system/system_call.R
+++ R/file_system/system_call.R
@@ -14,14 +14,21 @@ list.files()
 dd <- system('ls -al', intern = T)
 dd
 
+library(tidyverse)
+
 getwd()
+
+system('ls -al')
 
 list.files(R.home(), full.names = T)
 
 library(tidyverse)
 
-
+copy_origin
 copy_origin <- list.files(getwd(), full.names = T)[str_detect(list.files(), '^index.html$')]
 file.copy(copy_origin, "../docs/index.html", overwrite = T, copy.date = T)
-
-
+search_file <- str_detect(list.files(full.names = T, recursive = T), 
+                          'index.html|credit_card.nb.html|file_system.nb.html|normal_distribution.nb.html|interest_rate_risk.nb.html|covariance.nb.html')
+copy_origin <- list.files(full.names = T, recursive = T)[search_file]
+copy_origin
+file.copy(copy_origin, "../docs/", overwrite = T, copy.date = T)
