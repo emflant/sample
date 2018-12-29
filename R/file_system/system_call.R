e4a086d033151c0fc266705fae1600f0284fe012
emflant <emflant@gmail.com>
2018-12-30 00:56:14 +0900

    .

--- R/file_system/system_call.R
+++ R/file_system/system_call.R
@@ -23,12 +23,15 @@ system('ls -al')
 list.files(R.home(), full.names = T)
 
 library(tidyverse)
+str_c(c("a", NA, "b"), "-d")
 
-copy_origin
-copy_origin <- list.files(getwd(), full.names = T)[str_detect(list.files(), '^index.html$')]
-file.copy(copy_origin, "../docs/index.html", overwrite = T, copy.date = T)
+# [1] "covariance.nb.html|credit_card.nb.html|file_system.nb.html|index.html|interest_rate_risk.nb.html|normal_distribution.nb.html"
+str_c(list.files('../docs', '\\.html$'), collapse = "|")
 search_file <- str_detect(list.files(full.names = T, recursive = T), 
-                          'index.html|credit_card.nb.html|file_system.nb.html|normal_distribution.nb.html|interest_rate_risk.nb.html|covariance.nb.html')
+                          str_c(list.files('../docs', '\\.html$'), collapse = "|"))
 copy_origin <- list.files(full.names = T, recursive = T)[search_file]
 copy_origin
+
 file.copy(copy_origin, "../docs/", overwrite = T, copy.date = T)
+
+
