3a83e8513e66f168916dcee50f26fdf601d50f18
emflant <emflant@gmail.com>
2019-01-09 22:09:13 +0900

    .

--- R/file_system/system_call.R
+++ R/file_system/system_call.R
@@ -26,12 +26,15 @@ library(tidyverse)
 str_c(c("a", NA, "b"), "-d")
 search_file
 # [1] "covariance.nb.html|credit_card.nb.html|file_system.nb.html|index.html|interest_rate_risk.nb.html|normal_distribution.nb.html"
-str_c(list.files('../docs', '\\.html$'), collapse = "|")
+
+list.files('../docs', pattern = '\\.html$', full.names = T)
+str_c(list.files('../docs', pattern = '\\.html$'), collapse = "|")
 search_file <- str_detect(list.files(full.names = T, recursive = T), 
                           str_c(list.files('../docs', '\\.html$'), collapse = "|"))
 copy_origin <- list.files(full.names = T, recursive = T)[search_file]
 copy_origin
 
 file.copy(copy_origin, "../docs/", overwrite = T, copy.date = T)
+
 
 
