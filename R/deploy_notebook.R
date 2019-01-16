3b14aa0f733e687130bcedafae1c9fcb25a00184
emflant <emflant@gmail.com>
2019-01-16 22:18:37 +0900

    .

---
+++ R/deploy_notebook.R
@@ -0,0 +1,9 @@
+library(stringr)
+# list.files('../docs', pattern = '\\.html$', full.names = T)
+# str_c(list.files('../docs', pattern = '\\.html$'), collapse = "|")
+search_file <- str_detect(list.files(full.names = T, recursive = T), 
+                          str_c(list.files('../docs', '\\.html$'), collapse = "|"))
+copy_origin <- list.files(full.names = T, recursive = T)[search_file]
+copy_origin
+
+file.copy(copy_origin, "../docs/", overwrite = T, copy.date = T)
