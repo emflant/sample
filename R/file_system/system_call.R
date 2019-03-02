8520bf18af9c7b6285bca22d9c3591ed7cd88220
emflant <emflant@gmail.com>
2019-03-02 18:10:42 +0900

    .

--- R/file_system/system_call.R
+++ R/file_system/system_call.R
@@ -1,34 +1,10 @@
-
-list.files()
-
-
-getwd()
-
-file.path(getwd(), 'doc/')
-setwd(file.path(getwd(), 'doc/'))
-
-setwd('../docs')
-getwd()
-list.files()
-dd <- system('ls -al', intern = T)
-dd
-
 library(tidyverse)
 
-getwd()
-
-system('ls -al')
-
-list.files(R.home(), full.names = T)
-
-library(tidyverse)
-str_c(c("a", NA, "b"), "-d")
-search_file
-# [1] "covariance.nb.html|credit_card.nb.html|file_system.nb.html|index.html|interest_rate_risk.nb.html|normal_distribution.nb.html"
-
-
+# copy list
 list.files('../docs', pattern = '\\.html$', full.names = T)
 str_c(list.files('../docs', pattern = '\\.html$'), collapse = "|")
+
+# copy list
 search_file <- str_detect(list.files(full.names = T, recursive = T), 
                           str_c(list.files('../docs', '\\.html$'), collapse = "|"))
 copy_origin <- list.files(full.names = T, recursive = T)[search_file]
