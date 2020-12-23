785ae5aff915cb344af63fb8631ff375ce6cefea
emflant <emflant@gmail.com>
2020-12-24 00:56:29 +0900

    .

---
+++ docker/rocker/tidyverse:4.0.0-ubuntu18.04/build/build_tidyverse.sh
@@ -0,0 +1,2 @@
+docker rmi rocker/tidyverse-1:4.0.0-ubuntu18.04
+docker build -t rocker/tidyverse-1:4.0.0-ubuntu18.04 .
--- docker/rocker/tidyverse:4.0.0-ubuntu18.04/run_rocker.sh
+++ docker/rocker/tidyverse:4.0.0-ubuntu18.04/run_rocker.sh
@@ -1,1 +1,1 @@
-docker run --rm --name my-rocker -p 8787:8787 -e PASSWORD=1111 rocker/tidyverse:4.0.0-ubuntu18.04
+docker run --rm --name my-rocker -p 8787:8787 -e PASSWORD=1111 rocker/tidyverse-1:4.0.0-ubuntu18.04
