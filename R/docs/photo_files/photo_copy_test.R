af93146dc8810f35a012e7fc8318fae1d0b45b6d
emflant <emflant@gmail.com>
2021-04-28 22:11:54 +0900

    .

--- R/docs/photo_files/photo_copy_test.R
+++ R/docs/photo_files/photo_copy_test.R
@@ -30,8 +30,14 @@ copy_photo_files("~/Desktop/photo/input", "~/Desktop/photo/output")
 # 파일체크 (파일명, 파일사이즈 기준)
 # output 폴더와 외장하드 디스크 비교.
 re1 = file_compare("/Volumes/seagate/02_photo/2021", "~/Desktop/photo/output") 
-# re1
 
+# pc에는 있고, seagate 는 없는 디렉토리 목록.
+# 이건 ctrl+c,v 로 복사함.
+re1$path2 %>% 
+  mutate(dir_date = str_sub(dir_name, 1, 43)) %>% 
+  distinct(dir_date)
+
+#
 
 ##################################################################################
 # seagate <--> SAMSUNG 외장하드로 동기화
@@ -46,22 +52,31 @@ re2
 copy_sync("/Volumes/seagate/02_photo", "/Volumes/SAMSUNG/02_photo")
 ##################################################################################
 
-re2$path2 %>% 
-  group_by(to_path) %>% 
-  summarise(n()) %>% 
-  print(n = Inf)
 
 
 
-
-
-copy_sync()
-
-re2 = file_compare2("/Volumes/seagate/02_photo/etc", "/Volumes/SAMSUNG/02_photo/etc") 
+##################################################################################
+# seagate <--> disk01 외장하드로 동기화
+# 파일체크 (파일명, 파일사이즈 기준)
+re1 = file_compare("/Volumes/seagate/02_photo", "/Volumes/PhotoDisk/02_photo") 
+re1
+# 파일비교2 (파일경로, 파일명 기준)
+re2 = file_compare2("/Volumes/seagate/02_photo", "/Volumes/PhotoDisk/02_photo") 
 re2
+
 re2$path1 %>% 
-  filter(!str_detect(full_path, '2020-10-01|2020-10-24|2020-12-13'))
+  mutate(dir_date = str_sub(full_path, 1, 42)) %>% 
+  distinct(dir_date)
 
+re2$path1 %>% 
+  mutate(dir_date = str_sub(full_path, 1, 30)) %>% 
+  distinct(dir_date)
 
-dir.create("/Volumes/SAMSUNG/02_photo/2020/2020-10-01/JPG", recursive = T)
-file.copy("/Volumes/SAMSUNG/02_photo/2020/2020-10-01/JPG/P1300958.JPG", "/Volumes/seagate/02_photo/2020/2020-10-01/JPG/", overwrite = F, copy.date = T)
+re2$path2 %>% 
+  mutate(dir_date = str_sub(full_path, 1, 32)) %>% 
+  distinct(dir_date)
+  
+
+# 2개 폴더 동기화. 서로 없는거 copy
+copy_sync("/Volumes/seagate/02_photo", "/Volumes/PhotoDisk/02_photo")
+##################################################################################
