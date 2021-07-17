1e5fd9449ea349cc0e4b4ee62666ecc5b0096ffa
emflant <emflant@gmail.com>
2021-07-17 23:42:29 +0900

    .

---
+++ docker/python3/datascience-notebook/bridge_run_python3.sh
@@ -0,0 +1,6 @@
+docker run --rm --name my-python-3.8.8 \
+	--network selenium-net \
+	-p 8888:8888 \
+	-e JUPYTER_ENABLE_LAB=yes \
+	-v $PWD/mount/work:/home/jovyan/work \
+	python-3.8.8
--- docker/python3/datascience-notebook/run_python3.sh
+++ docker/python3/datascience-notebook/run_python3.sh
@@ -1,5 +1,4 @@
 docker run --rm --name my-python-3.8.8 \
-	--network selenium-net \
 	-p 8888:8888 \
 	-e JUPYTER_ENABLE_LAB=yes \
 	-v $PWD/mount/work:/home/jovyan/work \
