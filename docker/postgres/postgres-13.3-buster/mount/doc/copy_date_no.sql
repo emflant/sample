b8a94a8a58fd2d6f0e9f2f0fb4b4bfebc2c277e7
emflant <emflant@gmail.com>
2021-08-21 00:29:27 +0900

    .

--- docker/postgres/postgres-13.3-buster/mount/doc/copy_date_no.sql
+++ docker/postgres/postgres-13.3-buster/mount/doc/copy_date_no.sql
@@ -1,4 +1,4 @@
-COPY bd_card_dtl
+COPY no_card_dtl
 FROM '/var/lib/postgresql/my-data/exportdata/card_dtl.csv'
 DELIMITER ','
 QUOTE '"'
---
+++ docker/postgres/postgres-13.3-buster/mount/doc/explain_no.sql
@@ -0,0 +1,4 @@
+explain (format yaml)
+select *
+from no_card_dtl
+where pay_date = '2021-01-16' ;
