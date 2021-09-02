791362843d6dffcb16c6fcb9c651d8a3bd532d03
emflant <emflant@gmail.com>
2021-09-02 23:19:51 +0900

    .

--- docker/ruby/ruby-3.0.2-bullseye/run_jekyll.sh
+++ docker/ruby/ruby-3.0.2-bullseye/run_jekyll.sh
@@ -1,4 +1,5 @@
 docker run -it --rm --name my-ruby \
 	-p 4000:4000 \
        	-v ~/sample/ruby/jekyll:/usr/src/app/jekyll \
-	-w /usr/src/app/jekyll ruby-1 zsh
+	-v ~/github:/usr/src/app/github \
+	-w /usr/src/app/ ruby-1 zsh
