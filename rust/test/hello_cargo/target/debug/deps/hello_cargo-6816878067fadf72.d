82ef91328ef1117f87550da7bbe445b8c7ada949
emflant <emflant@gmail.com>
2021-09-12 14:27:54 +0900

    .

--- rust/test/hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.d
+++
@@ -1,5 +0,0 @@
-/Volumes/PhotoDisk/30_workspace/sample/rust/test/hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.rmeta: src/main.rs
-
-/Volumes/PhotoDisk/30_workspace/sample/rust/test/hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.d: src/main.rs
-
-src/main.rs:
--- rust/test/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/usr/src/myapp/test/hello_cargo/target/debug/hello_cargo: /usr/src/myapp/test/hello_cargo/src/main.rs
