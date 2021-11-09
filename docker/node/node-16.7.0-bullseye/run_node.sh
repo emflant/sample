d22ac239bea770ea356f441181d810fdb24f92cf
“emflant” <emflant@gmail.com>
2021-11-09 23:43:27 +0900

    .

--- docker/node/node-16.7.0-bullseye/run_node.sh
+++ docker/node/node-16.7.0-bullseye/run_node.sh
@@ -1,4 +1,5 @@
 docker run -it --rm --name my-node \
+	-p 5000:5000 \
 	-p 3000:3000 \
-       	-v ~/github/sample/nodejs:/usr/src/app \
+	-v ~/github/sample/nodejs:/usr/src/app \
 	-w /usr/src/app node-1 zsh
