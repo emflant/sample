965d3251a614fe358a8b0ec5635df4761daf8bd4
“emflant” <emflant@gmail.com>
2021-12-04 22:36:53 +0900

    .

--- java/spring/app02/src/main/java/com/example/app02/App02Application.java
+++ java/spring/app02/src/main/java/com/example/app02/App02Application.java
@@ -1,11 +1,14 @@
 package com.example.app02;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication
 @Controller
@@ -14,10 +17,18 @@ public class App02Application {
     public static void main(String[] args) {
         SpringApplication.run(App02Application.class, args);
     }
+    private static final Logger log = LoggerFactory.getLogger(App02Application.class);
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/signup")
+    public String signupForm(Model model) {
+        model.addAttribute("member", new Member());
+        return "member";
+    }
+
+    @PostMapping("/signup")
+    public String signupSubmit(@ModelAttribute Member member, Model model) {
+        log.info(member.toString());
+        model.addAttribute("member", member);
+        return "member";
     }
 }
---
+++ java/spring/app02/src/main/java/com/example/app02/Member.java
@@ -0,0 +1,104 @@
+package com.example.app02;
+
+public class Member {
+
+    private long id;
+    private String parentsName; // 부모님 이름
+    private String parentsTel;  // 부모님 전화번호
+    private String name;    // 이름
+    private String gender;  // 성별
+    private String birth;   // 생년월일
+    private String membershipDate; // 가입일자
+    private String tueClass;
+    private String wenClass;
+    private String thrClass;
+
+    public String getTueClass() {
+        return tueClass;
+    }
+
+    public void setTueClass(String tueClass) {
+        this.tueClass = tueClass;
+    }
+
+    public String getWenClass() {
+        return wenClass;
+    }
+
+    public void setWenClass(String wenClass) {
+        this.wenClass = wenClass;
+    }
+
+    public String getThrClass() {
+        return thrClass;
+    }
+
+    public void setThrClass(String thrClass) {
+        this.thrClass = thrClass;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getParentsName() {
+        return parentsName;
+    }
+
+    public void setParentsName(String parentsName) {
+        this.parentsName = parentsName;
+    }
+
+    public String getParentsTel() {
+        return parentsTel;
+    }
+
+    public void setParentsTel(String parentsTel) {
+        this.parentsTel = parentsTel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getMembershipDate() {
+        return membershipDate;
+    }
+
+    public void setMembershipDate(String membershipDate) {
+        this.membershipDate = membershipDate;
+    }
+
+    @Override
+    public String toString(){
+        return String.format(
+                "Customer[id=%d, parentsName='%s', parentsTel='%s', " +
+                        "name='%s', gender='%s', birth='%s', membershipDate='%s', " +
+                        "tueClass='%s', wenClass='%s', thrClass='%s']",
+                id, parentsName, parentsTel, name, gender, birth, membershipDate, tueClass, wenClass, thrClass);
+    }
+}
