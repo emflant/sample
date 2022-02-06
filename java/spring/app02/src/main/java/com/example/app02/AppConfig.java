e4a9cdffaed2c6f7f5aa9bb2824facb7a68cf8af
“emflant” <emflant@gmail.com>
2022-02-06 19:48:42 +0900

    .

--- java/spring/app02/src/main/java/com/example/app02/AppConfig.java
+++ java/spring/app02/src/main/java/com/example/app02/AppConfig.java
@@ -12,9 +12,15 @@ public class AppConfig {
     /*
      * Use the standard Mongo driver API to create a com.mongodb.client.MongoClient instance.
      */
+//    @Bean
+//    public MongoClient mongoClient() {
+//        return MongoClients.create("mongodb://my-mongo:27017");
+//    }
+
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://my-mongo:27017");
+        String url = "";
+        return MongoClients.create(url);
     }
 
 //    public @Bean MongoTemplate mongoTemplate() {
