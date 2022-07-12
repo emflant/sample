ce2228b4589bbe747d13254e15760b1b75da9be2
“emflant” <emflant@gmail.com>
2022-07-13 00:17:21 +0900

    .

---
+++ R/test/ggplot-histogram-02.R
@@ -0,0 +1,54 @@
+
+
+subway = read_excel(path = "~/data/ggplot2/subway_20220531.xlsx")
+holiday = tibble(date = c("2022-01-31", "2022-02-01", "2022-02-02", "2022-03-01", "2022-03-09", "2022-05-05")) %>% 
+  mutate(date = ymd(date), code = "h")
+holiday
+subway1 = subway %>% janitor::clean_names() 
+
+tb_time = subway1 %>% 
+  filter(yeogmyeong == '건대입구', gubun == '하차',
+         naljja == as.Date('2022-01-01'), hoseon == 2) %>%  
+  pivot_longer(cols = starts_with("x"), names_to = "time") %>% 
+  distinct(time) %>% 
+  mutate(time_dbl = 6:24) %>% 
+  print()
+
+subway2 = subway1 %>% 
+  filter(yeogmyeong == '건대입구', gubun == '하차',
+           naljja >= as.Date('2022-05-01') ) %>% 
+  group_by(naljja, yeogmyeong) %>% 
+  summarise_at(.vars = vars(x06si_ijeon:x23si_ihu), sum) %>% 
+  pivot_longer(cols = starts_with("x"), names_to = "time") %>% 
+  left_join(tb_time, by = "time") %>% 
+  mutate(times = map2(time_dbl, value, rep),
+         naljja = ymd(naljja)) %>% 
+  mutate(wday = wday(naljja, label = T), .after = "naljja") %>% 
+  mutate(week = epiweek(naljja), .after = "wday") %>% 
+  unnest_longer(times) %>% 
+  left_join(holiday, by = c("naljja"="date")) %>% 
+  mutate(holiday = code=="h"|wday =="Sat"|wday == "Sun") %>% 
+  mutate(holiday = !is.na(holiday)) %>% 
+  print(n = 100)
+
+
+ggplot(subway2,aes(x = times, y = after_stat(count))) +
+  # geom_histogram(binwidth = 1) +
+  # geom_bar(alpha = .3, fill = "gray0") +
+  geom_bar(aes(fill = holiday), alpha = .5) +
+  geom_density(adjust = 1.5) +
+  scale_x_continuous(limits = c(5,25), breaks = seq(6,24,6)) +
+  scale_fill_manual(values = c("#4E89AE", "#ED6663")) +
+  facet_grid(week ~ wday) +
+  theme_minimal(base_family = "AppleSDGothicNeo-ExtraBold", base_size = 13) +
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
+        legend.position = "none",
+        panel.grid.major.y = element_line(colour = "gray80"),
+        axis.title = element_blank(),
+        plot.margin = margin(.7,1,.7,1,"cm"),
+        plot.background = element_rect(fill = "#F1F0EA", color = "#F1F0EA")) 
+
+
+ggsave(paste0("./test/ggplot/save_ggplot_.png"),
+       width = 6, height = 6, dpi = 320, units = "in")
--- R/test/ggplot-subway.R
+++ R/test/ggplot-subway.R
@@ -9,46 +9,121 @@ holiday
 subway1 = subway %>% 
   janitor::clean_names() 
 subway1
+
+
 subway2 = subway1 %>% 
-  filter(yeogmyeong == '건대입구') %>% 
+  filter(yeogmyeong == '건대입구', gubun == '하차') %>% 
   mutate(date = ymd(naljja), .before = "naljja") %>% 
   mutate(wday = wday(date, label = T), .after = "date") %>% 
   mutate(ym = as.character(naljja, format = "%Y-%m"), .after = "wday") %>% 
-  filter(wday != "Sun", wday != "Sat") %>% 
-  anti_join(holiday, by = "date") %>% 
-  group_by(ym, gubun, hoseon) %>% 
+  # filter(wday != "Sun", wday != "Sat") %>% 
+  left_join(holiday, by = "date", keep = T) %>% 
+  group_by(ym, hoseon) %>% 
   mutate(cnt = n(), .after = "ym") %>% 
+  ungroup() %>% 
   select(-naljja, -yeogbeonho, -yeonbeon)
 subway2
-subway2 %>% 
-  filter(ym == "2022-01")
+
 subway3 = subway2 %>% 
   pivot_longer(cols = starts_with("x"), names_to = "times") %>% 
-  group_by(yeogmyeong, ym, cnt, date, wday, gubun) %>% 
+  group_by(yeogmyeong, ym, cnt, date.x, wday, date.y, gubun) %>% 
   summarise(total = sum(value), .groups = "drop") %>% 
-  filter(gubun == "하차")
-subway3
+  mutate(week = week(date.x), .after = "date.y")
+subway3 %>% print(n = 35) 
+
+subway3 %>% filter(week >= 12, week < 15) %>% print(n = Inf)
+  
 # v_date = subway3 %>% pull(naljja) %>% head(1)
-ggplot(subway3, aes(date, total)) +
+ggplot(subway3, aes(date.x, total, group = 1)) +
   geom_line()
 
-subway4 = subway3 %>% 
-  group_by(ym) %>% 
-  mutate(total_month = sum(total)) %>% 
-  mutate(total_day = total_month / cnt) %>% 
-  distinct(ym, cnt, total_day)
 
-ggplot(subway4, aes(ym, total_day, group = 1)) +
-  geom_line()
-ymd(v_date) %>% date
+subway3_week = subway3 %>% 
+  group_by(yeogmyeong, week) %>% 
+  summarise(date = min(date.x), total = sum(total), .groups = "drop") %>% 
+  head(-1)
 
-as.character(v_date, format = "%Y-%m")
-subway4
-subway4 = subway3 %>% 
-  mutate(ym = as.character(naljja, format = "%Y-%m")) %>% 
-  group_by(yeogmyeong, ym, gubun) %>% 
-  summarise(total = sum(total), .groups = "drop")
-# remove(subway4)
-ggplot(subway4, aes(x = ym, y = total)) +
-  geom_line(aes(group = gubun, colour = gubun)) +
-  theme_bw(base_family = "AppleSDGothicNeo-ExtraBold", base_size = 10)
+subway3_week
+
+ggplot(subway3_week, aes(date, total)) +
+  geom_line() + 
+  geom_smooth(method = lm, se = F) 
+
+ggplot(subway3_week, aes(date, total)) +
+  # geom_line(alpha = .5, colour = "red", size = 1) +
+  geom_line(colour = "gray20", size = .7, alpha = .8) + 
+  geom_smooth(method = lm, #method.args = list(degree = 2),
+              se = T, formula = y ~ x, size = 1.2,
+              colour = "#197163", fill = "#197163",
+              alpha = 0.3) +
+  scale_y_continuous(limits = c(200000, 450000), 
+                     breaks = seq(200000, 450000, 50000), 
+                     labels = as.character(seq(20, 45, 5))) +
+  theme_minimal(base_family = "AppleSDGothicNeo-ExtraBold", base_size = 13) +
+  theme(plot.background = element_rect(fill = "#F1F0EA", color = "#F1F0EA"),
+        panel.grid = element_blank(),
+        panel.grid.major.y = element_line(colour = "gray80"),
+        axis.title = element_blank(),
+        plot.margin = margin(.7,1,.7,1,"cm"),)
+
+################################################################
+g_smooth = function(in_method, in_colour1, in_colour2){
+  ggplot(subway3_week, aes(date, total)) +
+    # geom_line(alpha = .5, colour = "red", size = 1) +
+    geom_line(colour = "gray20", size = .7, alpha = .7) + 
+    geom_smooth(method = in_method, #method.args = list(degree = 2),
+                se = T, formula = y ~ x, size = 1, alpha = 0.5,
+                # colour = "#0AA1DD", fill = "#79DAE8", # 43658B
+                colour = in_colour1, fill = in_colour2) +
+    scale_y_continuous(limits = c(200000, 450000), 
+                       breaks = seq(200000, 450000, 50000), 
+                       labels = as.character(seq(20, 45, 5))) +
+    labs(title = paste0("method = ", in_method)) +
+    theme_minimal(base_family = "AppleSDGothicNeo-ExtraBold", base_size = 12) +
+    theme(plot.background = element_rect(fill = "#F1F0EA", color = "#F1F0EA"),
+          panel.grid = element_blank(),
+          panel.grid.major.y = element_line(colour = "gray80"),
+          axis.title = element_blank(),
+          plot.margin = margin(.7,1,.7,1,"cm"),
+          plot.title = element_text(size = 11))
+}
+
+g_smooth("lm","#1C658C", "#79DAE8") +
+g_smooth("loess","#91091E", "#F37878") +
+  plot_layout(ncol = 1) +
+  plot_annotation(title = "[ggplot2 - geom_smooth]",
+                  caption = "twitter @sourcebox7",
+                  theme = theme(plot.background = element_rect(fill = "#F1F0EA", color = NA),
+                                plot.title = element_text(family = "AppleSDGothicNeo-ExtraBold",
+                                                          margin = margin(0.5,0,0,0,"cm")),
+                                plot.caption = element_text(color = "gray30", 
+                                                            family = "Menlo", 
+                                                            hjust = 0.98, 
+                                                            size = 8,
+                                                            margin = margin(0,0,0.5,0,"cm"))))
+
+
+ggsave(paste0("./test/ggplot/save_ggplot_subway_05.png"),
+       width = 6, height = 6, dpi = 320, units = "in")
+
+# ggplot(subway3_week, aes(date, total)) +
+#   # geom_line(alpha = .5, colour = "red", size = 1) +
+#   geom_line(colour = "#FD5F00", size = 1, alpha = .3) + 
+#   geom_smooth(method = "loess", method.args = list(degree = 2),
+#               se = T, formula = y ~ x, 
+#               colour = "#005792", fill = "#005792",
+#               alpha = 0.3) +
+#   scale_y_continuous(limits = c(200000, 450000), 
+#                      breaks = seq(200000, 450000, 50000), 
+#                      labels = as.character(seq(20, 45, 5))) +
+#   theme_minimal(base_family = "AppleSDGothicNeo-ExtraBold") +
+#   theme(plot.background = element_rect(fill = "#F1F0EA", color = "#F1F0EA"),
+#         panel.grid = element_blank(),
+#         panel.grid.major.y = element_line(colour = "gray80"),
+#         axis.title = element_blank(),
+#         plot.margin = margin(.7,1,.7,1,"cm"),)
+
+
+# spline()
+# (z <- poly(1:10, 3))
+
