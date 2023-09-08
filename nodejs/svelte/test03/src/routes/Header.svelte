4be82685f7278504c40d25a061c796814ed6ac42
“emflant” <emflant@gmail.com>
2023-09-08 22:51:38 +0900

    .

--- nodejs/svelte/test03/src/routes/Header.svelte
+++ nodejs/svelte/test03/src/routes/Header.svelte
@@ -1,44 +1,40 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+	// import logo from '$lib/images/svelte-logo.svg';
+	// import github from '$lib/images/github.svg';
 </script>
 
-<header>
-	<div class="corner">
+<header>  
+	<!-- <div class="corner">
 		<a href="https://kit.svelte.dev">
 			<img src={logo} alt="SvelteKit" />
 		</a>
-	</div>
+	</div> -->
+	<ul class="nav nav-pills">
+		<li class="nav-item">
+		  <a class= {$page.url.pathname === '/' ? 'nav-link active' : 'nav-link'}
+		  aria-current={$page.url.pathname === '/' ? 'page' : undefined} href="/">Home</a>
+		</li>
+		<li class="nav-item">
+		  <a class= {$page.url.pathname === '/about' ? 'nav-link active' : 'nav-link'} 
+		  aria-current={$page.url.pathname === '/about' ? 'page' : undefined} href="/about">About</a>
+		</li>
+	  </ul>
 
-	<nav>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<!-- <li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li> -->
-		</ul>
-	</nav>
-
-	<div class="corner">
+	<!-- <div class="corner">
 		<a href="https://github.com/sveltejs/kit">
 			<img src={github} alt="GitHub" />
 		</a>
-	</div>
+	</div> -->
 </header>
 
-<style>
+<!-- <style>
 	header {
 		display: flex;
 		justify-content: space-between;
 	}
 
-	.corner {
+	/* .corner {
 		width: 3em;
 		height: 3em;
 	}
@@ -55,13 +51,7 @@
 		width: 2em;
 		height: 2em;
 		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
+	} */
 
 
 	ul {
@@ -81,34 +71,7 @@
 		position: relative;
 		height: 100%;
 	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
+	/* a:hover {
 		color: var(--color-theme-1);
-	}
-</style>
+	} */
+</style> -->
