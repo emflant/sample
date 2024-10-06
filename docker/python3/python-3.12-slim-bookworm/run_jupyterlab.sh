6e482e3ee782088ccc2e65b264a81e123d846ca2
“emflant” <emflant@gmail.com>
2024-10-06 22:54:43 +0900

    .

--- docker/python3/python-3.12-slim-bookworm/run_jupyterlab.sh
+++ docker/python3/python-3.12-slim-bookworm/run_jupyterlab.sh
@@ -1,5 +1,6 @@
 docker run -it --rm --name my-python-12 \
 	-p 8888:8888 \
+	-p 11434:11434 \
 	-v ~/github/sample/python3/docs/llm:/myfiles/docs \
 	-w /myfiles/ \
 	python-3.12-slim-bookworm zsh
--- docker/python3/python-3.12-slim-bookworm/run_python3.sh
+++
@@ -1,5 +0,0 @@
-docker run -it --rm --name my-python-12 \
-	-p 8888:8888 \
-	-v ~/github/sample/python3/docs:/home/jovyan/work \
-	-v ~/data:/home/jovyan/data \
-	python-3.12-slim-bookworm zsh
