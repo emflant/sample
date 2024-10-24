1fbb3778f728b1771474cd4148b99c2a5b0b5148
“emflant” <emflant@gmail.com>
2024-10-24 21:18:07 +0900

    .

--- docker/python3/python-3.12-slim-bookworm/run_jupyterlab.sh
+++ docker/python3/python-3.12-slim-bookworm/run_jupyterlab.sh
@@ -2,5 +2,6 @@ docker run -it --rm --name my-python-12 \
 	-p 8888:8888 \
 	-p 11434:11434 \
 	-v ~/github/sample/python3/docs/llm:/myfiles/docs \
+	-e OPENAI_TOKEN=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 \
 	-w /myfiles/ \
 	python-3.12 zsh
